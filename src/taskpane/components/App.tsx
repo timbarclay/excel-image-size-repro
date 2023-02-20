df913ab7efd549bfb3035625addcaa6c1072b3a3
Tim Barclay <timrbarclay@gmail.com>
2023-02-20 16:52:48 +0000

    Initial commit

---
+++ src/taskpane/components/App.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+import { DefaultButton } from "@fluentui/react";
+import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
+import Progress from "./Progress";
+import { logo } from "../image";
+
+/* global console, Excel, require  */
+
+export interface AppProps {
+  title: string;
+  isOfficeInitialized: boolean;
+}
+
+export interface AppState {
+  listItems: HeroListItem[];
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      listItems: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      listItems: [
+        {
+          icon: "Ribbon",
+          primaryText: "Achieve more with Office integration",
+        },
+        {
+          icon: "Unlock",
+          primaryText: "Unlock features and functionality",
+        },
+        {
+          icon: "Design",
+          primaryText: "Create and visualize like a pro",
+        },
+      ],
+    });
+  }
+
+  click = async () => {
+    try {
+      await Excel.run(async (context) => {
+        // Add image
+        const sheet = context.workbook.worksheets.getActiveWorksheet();
+
+        const shape = sheet.shapes.addImage(logo.data);
+        shape.height = 200;
+        shape.width = 300;
+
+        const range = sheet.getRange("A1");
+        range.format.rowHeight = 210;
+
+        await context.sync();
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    const { title, isOfficeInitialized } = this.props;
+
+    if (!isOfficeInitialized) {
+      return (
+        <Progress
+          title={title}
+          logo={require("./../../../assets/logo-filled.png")}
+          message="Please sideload your addin to see app body."
+        />
+      );
+    }
+
+    return (
+      <div className="ms-welcome">
+        <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Welcome" />
+        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
+          <p className="ms-font-l">
+            Modify the source files, then click <b>Run</b>.
+          </p>
+          <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
+            Run
+          </DefaultButton>
+        </HeroList>
+      </div>
+    );
+  }
+}
